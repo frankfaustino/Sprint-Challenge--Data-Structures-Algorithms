f2e191db3026d56cc63b9777b2873a8846283c61
unknown <alexisreyes90@gmail.com>
2018-08-25 12:36:38 -0400

    DFS recursively working with CB, start at roots, goes left until it cannot, then goes recursively right.

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -7,7 +7,24 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    # given this, and cb
+
+    #if there is a value, and it's not equal to none, put that value into the CB.
+
+    if not self.value == None:
+      cb(self.value)
+
+    # this.value is the value of left i.e traverse left tree recursively.
+
+    if self.left:
+      # we have given CB above self.value, so if we call cb(that has the value of left/ right) it can go to the left calling that value from cb
+      
+      self.left.depth_first_for_each(cb)
+
+    if self.right:
+      # if we go left, we have to have the abilitiy to go across the row
+
+      self.right.depth_first_for_each(cb)
 
   def breadth_first_for_each(self, cb):
     pass
@@ -26,11 +43,19 @@ class BinarySearchTree:
         self.right.insert(value)
 
   def contains(self, target):
+    # base case
+
     if self.value == target:
       return True
+
+    # call recursively 
+
     if self.left:
       if self.left.contains(target):
         return True
+
+    # if target doesn't exist in the left at any point
+
     if self.right:
       if self.right.contains(target):
         return True
